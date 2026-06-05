eac116a24bd97020fd015322315704a204cc822d
mohan srinivas reddy saragada <23BQ1A12A8@vvit.net>
2026-06-05 11:05:23 +0530

    Add Log function to send logs to API
    Implement logging function to send logs to external API.

--- logging-middleware/log.py
+++ logging-middleware/log.py
@@ -1,1 +1,31 @@
+import os
+import requests
 
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+
+LOG_API_URL = "http://20.244.56.144/evaluation-service/logs"
+
+def Log(stack, level, package, message):
+
+    payload = {
+        "stack": stack,
+        "level": level,
+        "package": package,
+        "message": message
+    }
+
+    headers = {
+        "Authorization": f"Bearer {ACCESS_TOKEN}",
+        "Content-Type": "application/json"
+    }
+
+    try:
+        response = requests.post(
+            LOG_API_URL,
+            json=payload,
+            headers=headers
+        )
+        return response.json()
+
+    except Exception as e:
+        print(f"Logging Failed: {e}")
